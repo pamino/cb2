ede96d8b6dc540025bc31ea8458444ffd1446e16
Tilman Dreßler <t.dre@kithara.com>
2023-05-14 21:48:14 +0200

    add lexer C1

--- src/lexer/c1.rs
+++ src/lexer/c1.rs
@@ -2,10 +2,90 @@ use logos::Logos;
 
 #[derive(Logos, Debug, PartialEq)]
 pub enum C1Token {
-    // TODO: Define variants and their token/regex
-
-    // Logos requires one token variant to handle errors,
-    // it can be named anything you wish.
+    #[regex(r"[ \n\r\t]", |_| logos::Skip)]
+    Whitespace,
+    #[regex(r"/\*[^(\*/)]*\*/", |_| logos::Skip)]
+    CComment,
+    #[regex(r"//.*", |_| logos::Skip)]
+    CppComment,
     #[error]
     Error,
+
+// keywords
+    #[token("bool")]
+    KwBoolean,
+    #[token("do")]
+    KwDo,
+    #[token("else")]
+    KwElse,
+    #[token("float")]
+    KwFloat,
+    #[token("for")]
+    KwFor,
+    #[token("if")]
+    KwIf,
+    #[token("int")]
+    KwInt,
+    #[token("printf")]
+    KwPrintf,
+    #[token("return")]
+    KwReturn,
+    #[token("void")]
+    KwVoid,
+    #[token("while")]
+    KwWhile,
+
+// operators
+    #[token("+")]
+    Plus,
+    #[token("-")]
+    Minus,
+    #[token("*")]
+    Asterisk,
+    #[token("/")]
+    Slash,
+    #[token("=")]
+    Assign,
+    #[token("==")]
+    Eq,
+    #[token("!=")]
+    Neq,
+    #[token("<")]
+    Lss,
+    #[token(">")]
+    Grt,
+    #[token("<=")]
+    Leq,
+    #[token(">=")]
+    Geq,
+    #[token("&&")]
+    And,
+    #[token("||")]
+    Or,
+
+// other tokens
+    #[token(",")]
+    Comma,
+    #[token(";")]
+    Semicolon,
+    #[token("(")]
+    LParen,
+    #[token(")")]
+    RParen,
+    #[token("{")]
+    LBrace,
+    #[token("}")]
+    RBrace,
+
+// term variables
+    #[regex(r"[0-9]+")]
+    ConstInt,
+    #[regex(r"([0-9]+\.[0-9]+|\.[0-9]+)([eE]([-+])?[0-9]+)?|[0-9]+[eE]([-+])?[0-9]+")]
+    ConstFloat,
+    #[regex(r"(true|false)")]
+    ConstBoolean,
+    #[regex(r#""([^"\\]|\\.)*""#)]
+    ConstString,
+    #[regex(r"([a-zA-Z])+([0-9]|[a-zA-Z])*")]
+    Id,
 }
